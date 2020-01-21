0b0119801008287479cddb739e59e3855706dd1d
umar-hasan <59401609+umar-hasan@users.noreply.github.com>
2020-01-21 17:02:34 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 
     
     $(window).resize(function(){
-        if (window.matchMedia("(min-width: 768px)").matches) {
+        if (window.matchMedia("(min-width: 992px)").matches) {
             if ($("#navbar-mobile-container").css('diplay') != 'none') {
                 $("#navbar-container").show();
                 $("#navbar-mobile-container").hide();
@@ -26,7 +26,7 @@ $(document).ready(function(){
     
 
     function initialBar() {
-        if (window.matchMedia("(min-width: 1200px)").matches) {
+        if (window.matchMedia("(min-width: 992px)").matches) {
             $("#navbar-container").fadeIn();
         }
         else {
@@ -44,10 +44,13 @@ $(document).ready(function(){
 
     $("#portfolio-card").click(function(){
         $("#navbar-container .portfolio-link").hide();
+        $("#portfolio-mobile").hide();
+
         $("#navbar-container .about-link").show();
         $(this).removeClass("fadeInLeft");
         $("#about-card").removeClass("fadeInDown");
         $("#resume-card").removeClass("fadeInRight");
+        aboutTransitionOut();
         $(this).addClass("fadeOutLeft");
         $("#about-card").addClass("fadeOutDown");
         $("#resume-card").addClass("fadeOutRight");
@@ -61,10 +64,9 @@ $(document).ready(function(){
     $("#about-card").click(function(){
         $("#navbar-container .about-link").hide();
         $("#navbar-container .portfolio-link").show();
-        $(this).removeClass("fadeInDown");
         $("#portfolio-card").removeClass("fadeInLeft");
         $("#resume-card").removeClass("fadeInRight");
-        $(this).addClass("fadeOutDown");
+        aboutTransitionOut();
         $("#portfolio-card").addClass("fadeOutLeft");
         $("#resume-card").addClass("fadeOutRight");
         
@@ -107,6 +109,8 @@ $(document).ready(function(){
 
         $(".about-link").click(function(){
             $("#portfolio-container").fadeOut();
+            $("#about-mobile").hide();
+            $("#portfolio-mobile").show();
             $(".about-link").fadeOut();
             $(".about-link").delay(500).hide(function(){
                 $("#navbar-container .portfolio-link").fadeIn();
@@ -118,8 +122,9 @@ $(document).ready(function(){
 
         $(".portfolio-link").click(function(){
             $("#about-container").fadeOut();
+            $("#portfolio-mobile").hide();
+            $("#about-mobile").show();
             $(".portfolio-link").fadeOut();
-            
             $("#about-container").delay(500).hide();
             $(".portfolio-link").delay(500).hide(function(){
                 $("#navbar-container .about-link").fadeIn();
@@ -145,7 +150,84 @@ $(document).ready(function(){
         $("#close-icon-wrapper .close").click(function(){
             $("#navbar-mobile-toggle").hide();
         });
+
+        $("#navbar-mobile-toggle").click(function(){
+            $("#navbar-mobile-toggle").hide();
+        });
+
+        $("#home-mobile").click(function(){
+            $("#navbar-mobile-toggle").hide();
+            $("#portfolio-card").removeClass("fadeOutLeft");
+            $("#resume-card").removeClass("fadeOutRight");
+            $("#portfolio-card").addClass("fadeInLeft");
+            $("#resume-card").addClass("fadeInRight");
+            aboutTransitionIn();
+            $("#navbar-container").fadeOut();
+            $("#navbar-container").delay(500).hide();
+            $("#navbar-mobile-container").delay(500).hide();
+            $("#home-container").delay(500).show();
+
+            if ($("#portfolio-container").css('display') != 'none') {
+                $("#portfolio-container").fadeOut();
+                $("#portfolio-container").delay(500).hide();
+            }
+
+            if ($("#about-container").css('display') != 'none') {
+                $("#about-container").fadeOut();
+                $("#about-container").delay(500).hide();
+            }
+        });
+
+        $("#about-mobile").click(function(){
+            $("#navbar-mobile-toggle").hide();
+            $("#about-mobile").hide();
+            $("#portfolio-mobile").show();
+            $("#portfolio-container").fadeOut();
+            $(".about-link").fadeOut();
+            $(".about-link").delay(500).hide(function(){
+                $("#navbar-container .portfolio-link").fadeIn();
+                $("#portfolio-container").delay(500).hide();
+                $("#about-container").fadeIn();
+            });
+            
+        });
+
+        $("#portfolio-mobile").click(function(){
+            $("#navbar-mobile-toggle").hide();
+            $("#portfolio-mobile").hide();
+            $("#about-mobile").show();
+            $("#about-container").fadeOut();
+            $(".portfolio-link").fadeOut();
+            $("#about-container").delay(500).hide();
+            $(".portfolio-link").delay(500).hide(function(){
+                $("#navbar-container .about-link").fadeIn();
+                $("#portfolio-container").fadeIn();
+            });
+            
+        });
     }
     
+
+    function aboutTransitionIn() {
+        $("#about-card").removeClass("fadeOutRight");
+        $("#about-card").removeClass("fadeOutDown");
+        if (window.matchMedia("(min-width: 1200px)").matches) {
+            $("#about-card").addClass("fadeInDown");
+        }
+        else {
+            $("#about-card").addClass("fadeInRight");
+        }
+    }
+
+    function aboutTransitionOut() {
+        $("#about-card").removeClass("fadeInDown");
+        $("#about-card").removeClass("fadeInRight");
+        if (window.matchMedia("(min-width: 1200px)").matches) {
+            $("#about-card").addClass("fadeOutDown");
+        }
+        else {
+            $("#about-card").addClass("fadeOutRight");
+        }
+    }
 
 });
